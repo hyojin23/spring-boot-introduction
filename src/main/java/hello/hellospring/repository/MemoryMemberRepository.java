8cbcc25e9d44dd0907f240076f1e96281e63f52b
hyojin <kimhj9292@gmail.com>
2022-10-14 23:47:36 +0900

    MemoryMemberRepositoryTest

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,18 +2,13 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-
-
-    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
@@ -33,7 +28,7 @@ public class MemoryMemberRepository implements MemberRepository{
     }
 
     @Override
-    public List<Memeber> findAll() {
-        return null;
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
     }
 }
