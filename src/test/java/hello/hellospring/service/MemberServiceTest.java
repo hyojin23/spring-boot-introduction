f786d47e819a54f8a396841244cc17b51480a945
hyojin <kimhj9292@gmail.com>
2022-10-21 23:59:11 +0900

    Add code to MemberServiceTest

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,13 +1,27 @@
 package hello.hellospring.service;
 
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
     @Test
-    void join() {
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+//        Assertions.assertThat()
+
     }
 
     @Test
